2075c0ba79c459f4c4eedd4e6e3d277aa13897f5
Francisco Juan <juansoussev@gmail.com>
2026-04-12 13:06:27 -0300

    Initial Commit

---
+++ DroidKit/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DroidKit
+//
+//  Created by Francisco Juan on 12/04/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
